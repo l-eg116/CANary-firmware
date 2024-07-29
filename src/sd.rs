417db844b803155891ed89eb3857147e599b8685
l-eg116 <legcom6@gmail.com>
2024-07-29 16:27:27 +0200

    Improved CanLogIterator::next error handling

--- src/sd.rs
+++ src/sd.rs
@@ -55,13 +55,14 @@ impl CanLogsInterator<'_> {
 impl Iterator for CanLogsInterator<'_> {
     type Item = Frame;
 
+    // TODO : improve error handling to skip invalid lines instead of ending iteration
     fn next(&mut self) -> Option<Self::Item> {
         while !self.log_file.is_eof() {
             if STORE_BUFFER_SIZE - self.stored.len() >= READ_BUFFER_SIZE {
                 let mut buffer = [0u8; READ_BUFFER_SIZE];
-                let read_count = self.log_file.read(&mut buffer).unwrap();
+                let read_count = self.log_file.read(&mut buffer).ok()?; // ? Read error
                 self.stored
-                    .push_str(core::str::from_utf8(&buffer[..read_count]).unwrap()) // TODO : error "buffer spilt a utf8 char"
+                    .push_str(core::str::from_utf8(&buffer[..read_count]).ok()?) // ? utf-8 char got split
                     .expect("it fits");
             }
 
@@ -79,17 +80,13 @@ impl Iterator for CanLogsInterator<'_> {
             let log_line = &stored_clone[..new_line_i];
             self.stored = String::from_str(&stored_clone[new_line_i + 1..]).expect("it fits");
 
-            let mut frame_bytes = log_line
-                .split(" ")
-                .last()
-                .unwrap() // TODO : error "empty line"
-                .split("#");
-            let frame_id = u16::from_str_radix(frame_bytes.next().unwrap(), 16).unwrap(); // TODO : error "expected at least 2 items" & "expected valid hex"
-            let frame_data = decode_hex(frame_bytes.next().unwrap()).unwrap(); // TODO : error "expected at least 2 items" & "expected valid hex"
+            let mut frame_bytes = log_line.split(" ").last()?.split("#");
+            let frame_id = u16::from_str_radix(frame_bytes.next()?, 16).ok()?; // ? frame_bytes doesn't have >= 1 elements || ? invalid hexadecimal
+            let frame_data = decode_hex(frame_bytes.next()?).ok()?; // ? frame_bytes doesn't have >= 2 elements || ? invalid hexadecimal
 
             return Some(Frame::new_data(
-                StandardId::new(frame_id).unwrap(), // TODO : error "unvalid frame_id"
-                frame_data.into_array::<8>().unwrap(), // TODO : error "frame_data should be exactly 8 bytes"
+                StandardId::new(frame_id)?,         // ? frame_id doesn't fit in 11 bits
+                frame_data.into_array::<8>().ok()?, // ? frame_data didn't contain 8 bytes
             ));
         }
         None
